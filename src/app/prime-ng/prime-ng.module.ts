26a45b2d082060e7f148dd42d0811dd995823750
Cristian Unigarro <ungcrisfit@gmail.com>
2024-07-21 11:54:51 -0500

    add: hero

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   exports: [
     // == PrimeNG Modules here == //
+    ButtonModule
   ]
 })
 export class PrimeNgModule { }
